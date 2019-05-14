e97621b9818210114af3d734f5eb50a9e7d00a59
Administrator <Administrator@ZHWSAA3456789>
2019-05-14 16:41:51 +0630

    first time c sharp test

---
+++ FirstCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
